04a7c7a26cc63df680884680953ca57f8a9aef98
hansrajshokal9252 / <8386913653Hr@>
2021-07-12 19:03:05 +0530

    advance initial comit

---
+++ app/src/main/java/com/hansraj/phonelogin/Activities/ActivityPhoneNumber.java
@@ -0,0 +1,42 @@
+package com.hansraj.phonelogin.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.hansraj.phonelogin.R;
+
+public class ActivityPhoneNumber extends AppCompatActivity {
+    EditText pnumber;
+    Button continuebutton;
+    String phoneNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_number);
+        getSupportActionBar().hide();
+        pnumber=findViewById(R.id.phoneBox);
+        continuebutton=findViewById(R.id.continueBtn);
+        continuebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                phoneNumber="+91"+pnumber.getText().toString().trim();
+                if(phoneNumber.length()!=13 ){
+                    pnumber.setError("enter a valid number");
+                }
+                else{
+                    Intent intent=new Intent(ActivityPhoneNumber.this, OtpActivity.class);
+                    intent.putExtra("phoneNumber", phoneNumber);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+    }
+}
